c7e566a9b1b65093c36ced782bca97e1321ddbc2
Oliullah <138612802+oliullah95@users.noreply.github.com>
2023-08-28 04:06:46 +0600

    third file adeded

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -1,58 +1,77 @@
-function calculateTriangleArea(){
-//    triangle base value
-const baseField = document.getElementById('triangle-base');
-const baseValueText = baseField.value;
-const base = parseFloat(baseValueText)
+function calculateTriangleArea() {
+    //    triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText)
 
 
-// get triangle height value\
-const heightField = document.getElementById('triangle-height')
-const heightValueText = heightField.value;
-const height = parseFloat(heightValueText);
-console.log(height);
-const area = 0.5 * base * height;
-// console.log(area);
+    // get triangle height value\
+    const heightField = document.getElementById('triangle-height')
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+    const area = 0.5 * base * height;
+    // console.log(area);
 
-// show triangle area
-const areaSpan = document.getElementById('triangle-area');
-areaSpan.innerText = area;
+    // show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
 }
 
-function calculateRectangleArea(){
+function calculateRectangleArea() {
     // get rectangle width
-   const widthField = document.getElementById('rectangle-width');
-   const widthValueText = widthField.value;
-   const width = parseFloat(widthValueText);
-//    console.log(width);
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    //    console.log(width);
 
-//    get rectangle length
-const lengthField =document.getElementById('rectangle-length');
-const lengthValueText = lengthField.value;
-const length = parseFloat(lengthValueText);
-// console.log(length);
+    //    get rectangle length
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    // console.log(length);
 
-// calculate area 
-const area = width * length;
+    // calculate area 
+    const area = width * length;
 
-// show rectangle area 
-const rectangleAreaSpan = document.getElementById('rectangle-area');
-rectangleAreaSpan.innerText = area;
+    // show rectangle area 
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
 
 // reusable function -----> reduce duplicate code
-function calculateParallelogramArea(){
+function calculateParallelogramArea() {
     const base = getInputValue('parallelogram-base');
-    console.log(base);
+    // console.log(base);
 
     const height = getInputValue('parallelogram-height');
-    console.log(height);
-    
+    // console.log(height);
+
     const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+
 }
+
+function calculateEllipseArea(){
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area',area);
+}
+
 // reusable get input value field i number
-function getInputValue(fieldId){
+function getInputValue(fieldId) {
     const inputField = document.getElementById(fieldId);
     const inputValueText = inputField.value;
     const value = parseFloat(inputValueText);
     return value;
 }
+
+// reusable set span, p, div, etc text 
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+    
+}
+
